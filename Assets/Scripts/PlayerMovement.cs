5164cf1bbb7988bf1ba519100f7a04ba9f57b678
danieliusbo <bodanielius@gmail.com>
2026-02-28 23:49:28 +0200

    Shift key action works

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,21 @@
+using UnityEditor.Rendering.LookDev;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace TopDown.Movement
+{
+    [RequireComponent(typeof(PlayerInput))]
+    public class PlayerMovement : Mover
+    {
+        private void OnMove(InputValue value)
+        {
+            Vector3 playerInput = new Vector3(value.Get<Vector2>().x, value.Get<Vector2>().y, 0);
+            currentInput = playerInput;
+        }
+
+        private void OnSprint(InputValue value)
+        {
+            isSprinting = value.isPressed;
+        }
+    }
+}
