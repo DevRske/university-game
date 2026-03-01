93d9411c393d93de04cfcb0849c04dcf3671a5ac
Furininkas <ugnius.jacinkevicius@gmail.com>
2026-03-01 21:37:13 +0200

    Moved to another folder, added check if prefab is not null

--- Assets/Scripts/Player/PlayerShoot.cs
+++ Assets/Scripts/Player/PlayerShoot.cs
@@ -6,15 +6,21 @@ public class PlayerShoot : MonoBehaviour
     [SerializeField] private GameObject projectilePrefab;
     [SerializeField] private float spawnOffset = 1f;
 
-    void Update()
+    private void Update()
     {
         if(Mouse.current.leftButton.wasPressedThisFrame)
         {
             Shoot();
         }
     }
-    void Shoot()
+    private void Shoot()
     {
+        if (projectilePrefab == null)
+        {
+            Debug.LogWarning("Projectile prefab is not assigned in PlayerShoot!");
+            return;
+        }
+
         Vector3 spawnPosition = transform.position + transform.up * spawnOffset;
         Instantiate(projectilePrefab, spawnPosition, transform.rotation);
     }
