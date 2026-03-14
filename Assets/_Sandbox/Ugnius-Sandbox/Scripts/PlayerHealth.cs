ec6d41353437f9d9106797c87fb9f158050f25fb
Furininkas <ugnius.jacinkevicius@gmail.com>
2026-03-14 15:22:54 +0200

    Implemented Player health, unfinished

---
+++ Assets/_Sandbox/Ugnius-Sandbox/Scripts/PlayerHealth.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using System;
+using Core.Systems;
+
+public class PlayerHealth : MonoBehaviour, IDamageable
+{
+    [SerializeField] private float maxHealth = 100f;
+    [SerializeField] private float currentHealth;
+
+    public PlayerState State { get; private set; } = PlayerState.Active;
+
+    public event Action<float, float> onHealthChanged;
+
+    private void Awake()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(float amount)
+    {
+        if (State == PlayerState.Eliminated)
+        {
+            return;
+        }
+
+        currentHealth = Mathf.Max(currentHealth - amount, 0f); // Prevents from health going below zero
+
+        onHealthChanged?.Invoke(currentHealth, maxHealth);
+
+        if (currentHealth <= 0f)
+        {
+            OnDeath();
+        }
+    }
+
+    private void OnDeath()
+    {
+        State = PlayerState.Eliminated;
+        gameObject.SetActive(false);
+    }
+}
