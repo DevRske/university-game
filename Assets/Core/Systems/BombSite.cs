80eab7120e5b971f9603965249dfbf95dc03df2a
Furininkas <ugnius.jacinkevicius@gmail.com>
2026-03-17 20:14:54 +0200

    Added BombSite system.

---
+++ Assets/Core/Systems/BombSite.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class BombSite : MonoBehaviour
+{
+    [SerializeField] private string siteID = "A";
+
+    public string SiteID => siteID;
+
+    public void OnBombPlanted()
+    {
+        Debug.Log($"[BombSite] Bomb planted at site {siteID}");
+    }
+
+    public void OnBombDefused()
+    {
+        Debug.Log($"[BombSite] Bomb defused at site {siteID}");
+    }
+}
